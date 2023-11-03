ccfab618e335a38c97a045425b19a9b4e08bf724
Catalina Titoveanu <catalina.titoveanu@scayle.com>
2023-11-03 14:24:34 +0200

    fixed review routes

--- packages/admin/routes/includes/reviews.php
+++ packages/admin/routes/includes/reviews.php
@@ -1,14 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Lunar\Hub\Http\Livewire\Pages\Products\ProductTypes\ProductTypeCreate;
-use Lunar\Hub\Http\Livewire\Pages\Products\ProductTypes\ProductTypeIndex;
-use Lunar\Hub\Http\Livewire\Pages\Products\ProductTypes\ProductTypeShow;
+use Lunar\Hub\Http\Livewire\Pages\Products\Reviews\ProductReviewCreate;
+use Lunar\Hub\Http\Livewire\Pages\Products\Reviews\ProductReviewIndex;
+use Lunar\Hub\Http\Livewire\Pages\Products\Reviews\ProductReviewShow;
 
 Route::group([
     'middleware' => 'can:catalogue:manage-products',
 ], function () {
-    Route::get('/', ProductTypeIndex::class)->name('hub.reviews.index');
-    Route::get('create', ProductTypeCreate::class)->name('hub.reviews.create');
-    Route::get('{productReview}', ProductTypeShow::class)->name('hub.reviews.show');
+    Route::get('/', ProductReviewIndex::class)->name('hub.reviews.index');
+    Route::get('create', ProductReviewCreate::class)->name('hub.reviews.create');
+    Route::get('{productReview}', ProductReviewShow::class)->name('hub.reviews.show');
 });
